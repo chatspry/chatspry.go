faf9e897745b36fd54d247e38a3cdfe11fee77d4
berwyn <berwyn.codeweaver@gmail.com>
2015-07-13 21:11:46 -0600

    Adds user resources

--- v1/v1.go
+++ v1/v1.go
@@ -7,14 +7,24 @@ import (
 	"net/http"
 )
 
-func createHTTPRequest(c *Client, stem string) (*http.Request, error) {
-	req, err := http.NewRequest("GET", c.URL+stem, nil)
+const (
+	httpGet   = "GET"
+	httpPatch = "PATCH"
+	httpPost  = "POST"
+	httpPut   = "PUT"
+)
+
+func createHTTPRequest(verb string, c *Client, stem string) (*http.Request, error) {
+	req, err := http.NewRequest(verb, c.URL+stem, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Authorization", "Bearer "+c.AuthKey)
+
+	if c.AuthKey != "" {
+		req.Header.Add("Authorization", "Bearer "+c.AuthKey)
+	}
 	return req, nil
 }
 
@@ -46,9 +56,55 @@ func Login(username, password string) error {
 	return errors.New("Not currently implemented!")
 }
 
+// CreateUser creates a new user. Does not require authentication.
+// See: http://docs.chatspry.apiary.io/#reference/user-accounts/user/register-a-new-user
+func (c *Client) CreateUser(u *User, scopes []string) (JSONObject, error) {
+	req, err := createHTTPRequest(httpGet, c, "/v1/user/"+*u.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var value JSONObject
+	err = parseBody(resp, value)
+	if err != nil {
+		return nil, err
+	}
+
+	return value, nil
+}
+
 // GetUser fetches a user with a given id from the API
+// See: http://docs.chatspry.apiary.io/#reference/user-accounts/user/display-an-existing-user
 func (c *Client) GetUser(id string) (*User, error) {
-	req, err := createHTTPRequest(c, "/v1/user/"+id)
+	req, err := createHTTPRequest(httpPut, c, "/v1/user/"+id)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var value User
+	err = parseBody(resp, value)
+	if err != nil {
+		return nil, err
+	}
+
+	return &value, nil
+}
+
+// UpdateUser will, given an authenticated client and user, update the
+// user and return the latest data from the server
+// See: http://docs.chatspry.apiary.io/#reference/user-accounts/user/change-an-existing-user
+func (c *Client) UpdateUser(u *User) (*User, error) {
+	req, err := createHTTPRequest(httpPatch, c, "/v1/users/"+*u.ID)
 	if err != nil {
 		return nil, err
 	}
@@ -68,16 +124,10 @@ func (c *Client) GetUser(id string) (*User, error) {
 }
 
 // GetCurrentConvoIDs will, given an authenticated client, request the
-// list of IDs that the current user is a part of in the form of
-// {
-//      "user_id": "38459979-e5f9-4c50-8bf6-8721bae33b4e",
-//      "convo_ids": [
-//          "e22200f1-3fc0-451a-a733-40681bcbc91f",
-//          "0f41b840-56ae-417b-bb44-b099273ce50c"
-//      ]
-// }
+// list of IDs that the current user is a part of
+// See: http://docs.chatspry.apiary.io/#reference/conversations/conversation-ids/list-all-conversation-ids
 func (c *Client) GetCurrentConvoIDs() (JSONObject, error) {
-	req, err := createHTTPRequest(c, "/convos/as/ids")
+	req, err := createHTTPRequest(httpGet, c, "/v1/convos/as/ids")
 	if err != nil {
 		return nil, err
 	}
